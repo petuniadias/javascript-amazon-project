32c88091dbcbe9c04e1617322ab26f62cd6f983a
Petunia <psofia20056@gmail.com>
2025-02-22 22:43:32 +0000

    add to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,8 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-name="${product.name}">
         Add to Cart
       </button>
     </div>
@@ -57,3 +58,16 @@ console.log(productsHTML);
 
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      console.log('Added product');
+      const productName = button.dataset.productName;
+      cart.push({
+        productName: productName,
+        quantity: 1
+      });
+      console.log(cart);
+    });
+  });
