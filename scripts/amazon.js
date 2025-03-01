30d1b7d8676f2aae123449e742758d97d18db941
Petunia <psofia20056@gmail.com>
2025-03-01 19:02:58 +0000

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,9 +80,18 @@ document.querySelectorAll('.js-add-to-cart')
           productId: productId,
           quantity: 1
         });
+
       }
 
+      let cartQuantity = 0;
 
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+      console.log(cartQuantity);
       console.log(cart);
     });
   });
