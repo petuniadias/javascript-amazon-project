46ce3107f44813066e253461b7b60094c464fa1c
Petunia <psofia20056@gmail.com>
2025-03-10 20:31:27 +0000

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Wednesday, June 15
     </div>
@@ -104,5 +104,10 @@ document.querySelectorAll('.js-delete-link')
       const productId = link.dataset.productId;
       removeFromCart(productId);
       console.log(cart);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
     }); 
   });
